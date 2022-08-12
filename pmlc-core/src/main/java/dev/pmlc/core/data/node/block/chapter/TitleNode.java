c2051cff9f7161466bde3774af9cf08bcfcc1e7f
Christian Neumanns <77769154+pml-lang@users.noreply.github.com>
2022-08-12 11:46:35 +0700

    ChapterTitle renamed to Title

--- pmlc-core/src/main/java/dev/pmlc/core/data/node/block/chapter/TitleNode.java
+++ pmlc-core/src/main/java/dev/pmlc/core/data/node/block/chapter/TitleNode.java
@@ -4,6 +4,7 @@ import dev.pmlc.core.data.formalnode.FormalPMLNode;
 import dev.pmlc.core.data.formalnode.block.chapter.FormalChapterTitleNode;
 import dev.pmlc.core.data.node.block.DocumentNode;
 import dev.pmlc.core.data.node.block.PMLBlockNode;
+import dev.pmlc.core.data.node.inline.TextNode;
 import dev.pp.basics.annotations.NotNull;
 
 public class ChapterTitleNode extends PMLBlockNode {
@@ -15,6 +16,13 @@ public class ChapterTitleNode extends PMLBlockNode {
 
 
     public ChapterTitleNode () { super(); }
+
+    public ChapterTitleNode ( @NotNull String text, int level ) {
+
+        super();
+        this.level = level;
+        addInlineChildNode ( new TextNode ( text ) );
+    }
 
 
     public @NotNull FormalPMLNode<Void, ChapterTitleNode> getFormalNode() { return FormalChapterTitleNode.NODE; }
